fedda23c6ce524f770030b89a02cfb99375a8d6e
danding <2434313056@qq.com>
2016-09-18 23:16:35 +0800

    使用了rxjava 进行改造 但是感觉不对劲 很怪

--- src/main/java/com/example/mysmall/caipiao/MainActivity.java
+++ src/main/java/com/example/mysmall/caipiao/MainActivity.java
@@ -4,8 +4,8 @@ import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
@@ -38,10 +38,17 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.functions.Func2;
+import rx.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-    private Set<CaiPiao> caiPiaos = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,146 +65,188 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void caipiao() {
-        new Thread(new Runnable() {
+        final Func0<Set<CaiPiao>> func0 = new Func0<Set<CaiPiao>>() {
             @Override
-            public void run() {
-                synchronized (MainActivity.this) {
+            public Set<CaiPiao> call() {
+                Set<CaiPiao> caiPiaos = new HashSet<>();
+                File path1 = new File(getExternalFilesDir("").getAbsolutePath() + File.separator + "caipiao.txt");
+                boolean pathIsExist = path1.exists() && path1.length() > 10000;
+                try {
+                    if (pathIsExist) {
+                        FileInputStream fileInputStream = new FileInputStream(path1);
+                        String content = new String(readInputStream(fileInputStream));
+                        caiPiaos = deSerialization(content);
+                    } else {
+                        StringBuffer stringBuffer = new StringBuffer();
 
-                    try {
-                        File path1 = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "caipiao.txt");
-                        if (path1.exists() && path1.length() > 10000) {
-                            FileInputStream fileInputStream = new FileInputStream(path1);
-                            String content = new String(readInputStream(fileInputStream));
-                            MainActivity.this.caiPiaos = deSerialization(content);
-                        } else {
-                            StringBuffer stringBuffer = new StringBuffer();
-
-                            String[] paths = getAssets().list("caipiao");
-                            System.out.println(Arrays.toString(paths));
-                            for (String path : paths) {
-                                InputStream input = getAssets().open("caipiao" + File.separator + path);
-                                byte[] by = new byte[input.available()];
-                                input.read(by);
-                                String text = new String(by, "GB2312");
-                                stringBuffer.append(text);
-                                input.close();
-                            }
-                            String string = stringBuffer.toString();
-                            String[] splite1 = string.split("\r\n");
-                            for (String content : splite1) {
-                                String[] danding = content.split("    ");
-
-                                caiPiaos.add(new CaiPiao(danding[0], Arrays.asList(danding[1].split("\\|")[0].split(",")), new Integer(danding[1].split("\\|")[1])));
-                            }
-
-                            xuliehua();
+                        String[] paths = getAssets().list("caipiao");
+                        for (String path : paths) {
+                            InputStream input = getAssets().open("caipiao" + File.separator + path);
+                            byte[] by = new byte[input.available()];
+                            input.read(by);
+                            String text = new String(by, "GB2312");
+                            stringBuffer.append(text);
+                            input.close();
                         }
+                        String string = stringBuffer.toString();
+                        String[] splite1 = string.split("\r\n");
+                        for (String content : splite1) {
+                            String[] danding = content.split("    ");
+
+                            caiPiaos.add(new CaiPiao(danding[0], Arrays.asList(danding[1].split("\\|")[0].split(",")), new Integer(danding[1].split("\\|")[1])));
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                boolean dan = false;
+
+                if(dan || !pathIsExist) {
+                    try {
+                        xuliehua(caiPiaos);
                     } catch (IOException e) {
                         e.printStackTrace();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-
-                    ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
-                    NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-                    if(networkInfo != null && networkInfo.isAvailable()) {
-                        try {
-                            URL url = new URL("http://f.apiplus.cn/ssq-50.json");
-
-                            URLConnection rulConnection = url.openConnection();// 此处的urlConnection对象实际上是根据URL的
-                            HttpURLConnection httpUrlConnection = (HttpURLConnection) rulConnection;
-                            httpUrlConnection.setRequestMethod("GET");
-                            httpUrlConnection.setConnectTimeout(15000);
-                            InputStream inputStream = httpUrlConnection.getInputStream();
-                            byte[] by = readInputStream(inputStream);
-                            String text = new String(by);
-
-                            JSONObject jsonObject = new JSONObject(text);
-                            JSONArray data = jsonObject.optJSONArray("data");
-
-                            boolean dan = true;
-                            for (int i = 0; i < data.length(); i++) {
-                                JSONObject jsonObject1 = data.optJSONObject(i);
-                                CaiPiao caiPiao = new CaiPiao(jsonObject1.optString("expect"),
-                                        Arrays.asList(jsonObject1.optString("opencode").split("\\+")[0].split(",")),
-                                        new Integer(jsonObject1.optString("opencode").split("\\+")[1]));
-
-                                if(MainActivity.this.caiPiaos.add(caiPiao)) {
-                                    dan = true;
-                                }
-                            }
-
-                            if(dan) {
-                                xuliehua();
-                            }
-                        } catch (Exception e) {
-                            Toast.makeText(MainActivity.this, "网络请求出错了  " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            e.printStackTrace();
-                        }
-                    }
-
-                    Random random = new Random();
-
-                    Set<String> myset = new HashSet<String>();
-
-                    final StringBuilder stringBuilder = new StringBuilder();
-                    for (int i = 0; i < 100; i++) {
-                        myset.add(random.nextInt(32) + 1 + "");
-                        if (myset.size() == 6) {
-                            List<Integer> hshs = new ArrayList();
-                            for (String ccc : myset) {
-                                hshs.add(new Integer(ccc));
-                            }
-                            Collections.sort(hshs);
-                            for (Integer inte : hshs) {
-                                stringBuilder.append(inte + "   ");
-                            }
-                            break;
-                        }
-                    }
-                    int blue = random.nextInt(15) + 1;
-                    stringBuilder.append(blue);
-                    final CaiPiao caiPiao = new CaiPiao("1", new ArrayList<String>(myset), blue);
-                    if (MainActivity.this.caiPiaos.contains(caiPiao)) {
-                        new Handler(getMainLooper()).post(new Runnable() {
-                            @Override
-                            public void run() {
-                                textView.setText("牛B的人生  " + caiPiao);
-                            }
-                        });
-
-                    } else {
-                        new Handler(getMainLooper()).post(new Runnable() {
-                            @Override
-                            public void run() {
-                                SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
-                                for (Integer inte : caiPiao.getReds()) {
-                                    String source = inte + "";
-                                    SpannableString  msp = new SpannableString(source);
-                                    msp.setSpan(new ForegroundColorSpan(Color.RED), 0, source.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                                    spannableStringBuilder.append(msp);
-                                    spannableStringBuilder.append("    ");
-                                }
-
-                                String blue = caiPiao.getBlue() + "";
-                                SpannableString  msp = new SpannableString(blue);
-                                msp.setSpan(new ForegroundColorSpan(Color.BLUE), 0, blue.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                                spannableStringBuilder.append(msp);
-                                textView.setText(spannableStringBuilder);
-                            }
-                        });
                     }
                 }
+                return caiPiaos;
             }
-        }).start();
+        };
 
+
+        final Func0<Set<CaiPiao>> func01 = new Func0<Set<CaiPiao>>() {
+            @Override
+            public Set<CaiPiao> call() {
+                Set caiPiaos = new HashSet();
+                ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if(networkInfo != null && networkInfo.isAvailable()) {
+                    try {
+                        URL url = new URL("http://f.apiplus.cn/ssq-50.json");
+
+                        URLConnection rulConnection = url.openConnection();// 此处的urlConnection对象实际上是根据URL的
+                        HttpURLConnection httpUrlConnection = (HttpURLConnection) rulConnection;
+                        httpUrlConnection.setRequestMethod("GET");
+                        httpUrlConnection.setConnectTimeout(15000);
+                        InputStream inputStream = httpUrlConnection.getInputStream();
+                        byte[] by = readInputStream(inputStream);
+                        String text = new String(by);
+
+                        JSONObject jsonObject = new JSONObject(text);
+                        JSONArray data = jsonObject.optJSONArray("data");
+
+                        for (int i = 0; i < data.length(); i++) {
+                            JSONObject jsonObject1 = data.optJSONObject(i);
+                            CaiPiao caiPiao = new CaiPiao(jsonObject1.optString("expect"),
+                                    Arrays.asList(jsonObject1.optString("opencode").split("\\+")[0].split(",")),
+                                    new Integer(jsonObject1.optString("opencode").split("\\+")[1]));
+
+                            caiPiaos.add(caiPiao);
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "网络请求出错了  " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+
+                return caiPiaos;
+            }
+        };
+
+        Observable.create(new Observable.OnSubscribe<Set<CaiPiao>>() {
+            @Override
+            public void call(Subscriber<? super Set<CaiPiao>> subscriber) {
+                subscriber.onStart();
+                subscriber.onNext(func0.call());
+                subscriber.onNext(func01.call());
+                subscriber.onCompleted();
+            }
+        }).scan(new Func2<Set<CaiPiao>, Set<CaiPiao>, Set<CaiPiao>>() {
+            @Override
+            public Set<CaiPiao> call(Set<CaiPiao> caiPiaos, Set<CaiPiao> caiPiaos2) {
+                System.out.println("淡定  kajshkj    ");
+                caiPiaos.addAll(caiPiaos2);
+                return caiPiaos;
+            }
+        })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .last().doOnNext(new Action1<Set<CaiPiao>>() {
+            @Override
+            public void call(Set<CaiPiao> caiPiaos) {
+                try {
+                    xuliehua(caiPiaos);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        })
+                .map(new Func1<Set<CaiPiao>, CharSequence>() {
+                    @Override
+                    public CharSequence call(Set<CaiPiao> caiPiaos) {
+
+                        Random random = new Random();
+
+                        Set<String> myset = new HashSet<String>();
+
+                        final StringBuilder stringBuilder = new StringBuilder();
+                        for (int i = 0; i < 100; i++) {
+                            myset.add(random.nextInt(32) + 1 + "");
+                            if (myset.size() == 6) {
+                                List<Integer> hshs = new ArrayList();
+                                for (String ccc : myset) {
+                                    hshs.add(new Integer(ccc));
+                                }
+                                Collections.sort(hshs);
+                                for (Integer inte : hshs) {
+                                    stringBuilder.append(inte + "   ");
+                                }
+                                break;
+                            }
+                        }
+                        int blue = random.nextInt(15) + 1;
+                        stringBuilder.append(blue);
+                        final CaiPiao caiPiao = new CaiPiao("1", new ArrayList<String>(myset), blue);
+
+                        if (caiPiaos.contains(caiPiao)) {
+                            return "牛B的人生  " + caiPiao;
+                        } else {
+                            SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
+                            for (Integer inte : caiPiao.getReds()) {
+                                String source = inte + "";
+                                SpannableString msp = new SpannableString(source);
+                                msp.setSpan(new ForegroundColorSpan(Color.RED), 0, source.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                spannableStringBuilder.append(msp);
+                                spannableStringBuilder.append("    ");
+                            }
+
+                            String blue1 = caiPiao.getBlue() + "";
+                            SpannableString msp = new SpannableString(blue1);
+                            msp.setSpan(new ForegroundColorSpan(Color.BLUE), 0, blue1.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            spannableStringBuilder.append(msp);
+                            return spannableStringBuilder.toString();
+                        }
+                    }
+                }).subscribe(new Action1<CharSequence>() {
+            @Override
+            public void call(final CharSequence s) {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(s);
+                    }
+                });
+
+            }
+        });
     }
 
-    private void xuliehua() throws IOException {
+    private void xuliehua(Set<CaiPiao> caiPiaos) throws IOException {
         String content = serialize(caiPiaos);
 
-        File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "caipiao.txt");
+        File path = new File(getExternalFilesDir("").getAbsolutePath() + File.separator + "caipiao.txt");
         if (!path.exists()) {
             path.createNewFile();
         }
