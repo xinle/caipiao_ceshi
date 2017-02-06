c2330e223835ff75d5c1332a6def9f595e53b9d3
xinle <1390847875@qq.com>
2017-02-07 00:00:26 +0800

    Update CaiPiao.java

--- src/main/java/com/example/mysmall/caipiao/CaiPiao.java
+++ src/main/java/com/example/mysmall/caipiao/CaiPiao.java
@@ -1,5 +1,11 @@
 package com.example.mysmall.caipiao;
 
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -52,11 +58,21 @@ public class CaiPiao implements Serializable ,Comparable<CaiPiao> {
 
     @Override
     public String toString() {
-        return "CaiPiao{" +
-                "date='" + date + '\'' +
-                ", reds=" + reds +
-                ", blue=" + blue +
-                '}';
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
+        for (Integer inte : getReds()) {
+            String source = inte + "";
+            SpannableString msp = new SpannableString(source);
+            msp.setSpan(new ForegroundColorSpan(Color.RED), 0, source.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spannableStringBuilder.append(msp);
+            spannableStringBuilder.append("    ");
+        }
+
+        String blue1 = getBlue() + "";
+        SpannableString msp = new SpannableString(blue1);
+        msp.setSpan(new ForegroundColorSpan(Color.BLUE), 0, blue1.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableStringBuilder.append(msp);
+
+        return spannableStringBuilder.toString();
     }
 
     @Override
@@ -67,6 +83,7 @@ public class CaiPiao implements Serializable ,Comparable<CaiPiao> {
         CaiPiao caiPiao = (CaiPiao) o;
 
         if (blue != caiPiao.blue) return false;
+
         return reds != null ? reds.equals(caiPiao.reds) : caiPiao.reds == null;
     }
 
